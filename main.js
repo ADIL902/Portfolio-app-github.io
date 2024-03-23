8e90d57ab39ced7c11b6d266e9722072414ab1b1
ADIL902 <144104491+ADIL902@users.noreply.github.com>
2024-03-24 01:21:48 +0530

    Build logic

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings:["Frontend Developer", "Php Developer", "Web Developer"],
+    typeSpeed : 10,
+    backSpeed : 100,
+    backDelay : 1000,
+    loop : true
+});
